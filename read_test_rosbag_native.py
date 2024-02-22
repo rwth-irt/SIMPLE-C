2de879d23be12062b3b6928d3cdeedd87e9fd661
Max Viehl <max.viehl@rwth-aachen.de>
2024-02-22 21:56:25 +0100

    Add numpy caching to import function

--- read_test_rosbag_native.py
+++ read_test_rosbag_native.py
@@ -2,8 +2,29 @@ import rosbag
 from sensor_msgs import point_cloud2
 import numpy as np
 
+from pathlib import Path
 
-def bag_to_numpy(filename, only_frames=None):
+
+def bag_to_numpy(filename, only_frames=None, cache=True):
+    """
+    Returns a 3d-numpy array.
+    1st dimension is frame over time
+    2nd dimension is point index
+    each point is stored as [x, y, z, intensity]
+
+    You can pass a list of frame numbers to read (speeding up import without cache significantly).
+    If None, all frames are imported.
+
+    Uses numpy files to cache slow imports, but only if all frames should be imported.
+    """
+    if only_frames:
+        cache = False
+        # otherwise we would save a limited selection to cache, leading to confusing imports later
+    cache_filename = filename + "_cache.npy"
+    if cache and Path(cache_filename).is_file():
+        print("using cache")
+        return np.load(cache_filename)
+
     print("starting import")
     out = []
     bag = rosbag.Bag(filename)
@@ -12,14 +33,21 @@ def bag_to_numpy(filename, only_frames=None):
         i += 1
         if only_frames and i - 1 not in only_frames:
             continue
-        out.append(np.array(point_cloud2.read_points_list(msg, skip_nans=True)))
+        out.append(np.array(point_cloud2.read_points_list(msg, skip_nans=False)))
     bag.close()
+    out = np.array(out)
     print("import done")
+    if cache:
+        print("writing cache file")
+        np.save(cache_filename, out)
     return out
 
 
-def visualize_frame(frame):
+def visualize_frame(frame_in):
     import open3d as o3d
+    
+    # remove nans from frame_in
+    frame = frame_in[~(np.isnan(frame_in).any(axis=1))]
 
     pcd = o3d.geometry.PointCloud()
     pcd.points = o3d.utility.Vector3dVector(frame[:, :3])
@@ -30,5 +58,5 @@ def visualize_frame(frame):
     o3d.visualization.draw_geometries([pcd])
 
 
-frames = bag_to_numpy("labor_lidar.bag", only_frames=[40])
+frames = bag_to_numpy("labor_lidar.bag")
 visualize_frame(frames[0])
