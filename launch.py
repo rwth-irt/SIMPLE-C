cc528f05fe901a50b643939aea05edc4faf614d3
Max Viehl <max.viehl@rwth-aachen.de>
2024-02-22 21:56:25 +0100

    only show n brightest points

--- launch.py
+++ launch.py
@@ -1,10 +1,24 @@
 from imports import bag_to_numpy
 from visualization import visualize_animation
+import numpy as np
 
 filename = (
     "/home/max/UNI/Job_IRT/LIDAR/temporal_reflector/temporal_reflector_disturbance.bag"
 )
 # filename = "labor_lidar.bag"
 frames = bag_to_numpy(filename)
+
+
+def process(frames):
+    # per frame, get the indices of the N points with highest intensity
+    N = 6
+    out = np.zeros((len(frames), N, 4))
+    selection = np.argsort(-frames[:, :, 3], axis=1)[:, :N]
+    for i, (sel, frame) in enumerate(zip(selection, frames)):
+        out[i] = frame[sel]
+        # don't know how to do this with one of those billion np functions, select, take, take_along_axis
+    return out
+
+
 # visualize_frame(frames[0])
-visualize_animation(frames)
+visualize_animation([process(frames), frames])
