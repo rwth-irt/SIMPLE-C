67ea5e69e7cfab25fd02bcf7f8e91e31caa26cbd
Max Viehl <max.viehl@rwth-aachen.de>
2024-04-10 15:13:05 +0200

    Add compression for cache files and print import progress

--- imports.py
+++ imports.py
@@ -4,9 +4,10 @@ import rosbag
 from sensor_msgs import point_cloud2
 import numpy as np
 from pathlib import Path
+import sys
 
 
-def bag_to_numpy(filename, only_frames=None, cache=True):
+def bag_to_numpy(filename, topic, only_frames=None, cache=True):
     """
     Returns a 3d-numpy array.
     1st dimension is frame over time
@@ -21,25 +22,29 @@ def bag_to_numpy(filename, only_frames=None, cache=True):
     if only_frames:
         cache = False
         # otherwise we would save a limited selection to cache, leading to confusing imports later
-    cache_filename = filename + "_cache.npy"
+    cache_filename = filename + "_cache.npz"
     if cache and Path(cache_filename).is_file():
         print("using cache")
-        return np.load(cache_filename)
+        return np.load(cache_filename)["data"]
 
     print("starting import")
     out = []
     bag = rosbag.Bag(filename)
+    msg_count = bag.get_message_count(topic_filters=topic)
     i = 0
-    for topic, msg, t in bag.read_messages(topics=["rslidar_points_ref"]):
+    for topic, msg, t in bag.read_messages(topics=[topic]):
         i += 1
+        sys.stdout.write("\r" + f"reading msg {i} / {msg_count}".rjust(30, " "))
+        sys.stdout.flush()
         if only_frames and i - 1 not in only_frames:
             continue
         out.append(np.array(point_cloud2.read_points_list(msg, skip_nans=False)))
     bag.close()
     out = np.array(out)
+    print()
     print("import done")
 
     if cache:
         print("writing cache file")
-        np.save(cache_filename, out)
+        np.savez_compressed(cache_filename, data=out)
     return out
--- launch.py
+++ launch.py
@@ -11,14 +11,21 @@ import numpy as np
 from pprint import pprint
 
 # MAIN
-filename = (
-    "/home/max/UNI/Job_IRT/LIDAR/temporal_reflector/temporal_reflector_disturbance.bag"
+filename1 = (
+    "/home/max/UNI/Job_IRT/LIDAR/temporal_reflector/temporal_reflector_disturbance.bag",
+    "rslidar_points_ref",  # topic
 )
-# filename = "labor_lidar.bag"
-frames = bag_to_numpy(filename)
+filename2 = (
+    "/home/max/UNI/Job_IRT/LIDAR/Calibration_Target_Deneb/calibration_target_1.bag",
+    "/rslidar_points_vr_v",  # topic
+)
+# get topics using `rosbag info filename.bag`
+
+
+frames = bag_to_numpy(*filename2)
 
 centers, visualization = get_cluster_centers_per_frame(
-    frames, rel_intensity_threshold=0.5, DBSCAN_epsilon=0.3, DBSCAN_min_samples=4
+    frames, rel_intensity_threshold=0.7, DBSCAN_epsilon=0.3, DBSCAN_min_samples=4
 )
 
 # chosen_centers = filter_clusters_1(centers, max_distance=0.3)
