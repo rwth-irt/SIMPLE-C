2887b157994de317d42dd70c38c5de48d3474fc9
Max Viehl <max.viehl@rwth-aachen.de>
2024-04-10 14:30:07 +0200

    Select bright points by threshold instead of N brightest

--- analysis.py
+++ analysis.py
@@ -5,49 +5,43 @@ from pprint import pprint
 
 def get_cluster_centers_per_frame(
     frames,  # input points: frames[frame_index, point_index] = [x,y,z,intensity]
-    N_highest_intensity=30,  # the N points with highest intens. used for DBSCAN clustering
+    rel_intensity_threshold,
     DBSCAN_epsilon=0.15,  # in meters, as is LIDAR output (TODO: is that correct?)
     DBSCAN_min_samples=3,
 ):
-    # per frame, get the indices of the N points with highest intensity
-    brightest_indices = np.argsort(-frames[:, :, 3], axis=1)[:, :N_highest_intensity]
-    # TODO maybe use a threshold instead
-
     # mark points for visualization, rewriting the intensity channel in `frames`
     # 0: any point
     # 1: for the N points with highest intensity
     # 2: part of a cluster
 
+    intensity_threshold = np.max(frames[..., 3]) * rel_intensity_threshold
+    point_selection = frames[..., 3] >= intensity_threshold
+
     visualization = np.copy(frames)
     visualization[:, :, 3] = 0  # reset intensity data
-    for i in range(len(frames)):
-        visualization[i, brightest_indices[i], 3] = 1  # 1 for brightest points
-
-    # get selected points in extra array
-    brightest_points = np.zeros((len(frames), N_highest_intensity, 4))
-    for i, (sel, frame) in enumerate(zip(brightest_indices, frames)):
-        brightest_points[i] = frame[sel]
-        # TODO don't know how to do this with one of those billion np functions, select, take, take_along_axis
+    visualization[point_selection, 3] = 1  # 1 for selected points
 
     cluster_centers_per_frame = []
     for i in range(len(frames)):
+        frame_points = frames[i, point_selection[i]]
         # perform DBSCAN per frame
         clusterlabels = DBSCAN(
             eps=DBSCAN_epsilon, min_samples=DBSCAN_min_samples
-        ).fit_predict(brightest_points[i, :, :3])
+        ).fit_predict(frame_points[:, :3])
 
         # get centroids of clusters and mark them for visualization
         centers = []
         for clusterlabel in np.unique(clusterlabels):
             if clusterlabel == -1:
                 continue  # ignore noise
-            points_in_cluster = frames[
-                i, brightest_indices[i, clusterlabels == clusterlabel]
-            ]
-            centroid = np.mean(points_in_cluster, axis=0)
-            centers.append(np.append(centroid, len(points_in_cluster)))
+            cluster_points = frame_points[clusterlabels == clusterlabel, :]
+            centroid = np.mean(cluster_points, axis=0)  # also calculate avg intensity
+            centers.append(np.append(centroid, len(cluster_points)))
 
-            visualization[i, brightest_indices[i, clusterlabels == clusterlabel], 3] = 2
+            # mark cluster points for visualization
+            in_frame_indices = np.nonzero(point_selection[i])[0]
+            in_cluster_indices = in_frame_indices[clusterlabels == clusterlabel]
+            visualization[i, in_cluster_indices, 3] = 2
 
         cluster_centers_per_frame.append(np.array(centers))
         # returns list of np array with one row per centroid, containing:
--- launch.py
+++ launch.py
@@ -1,6 +1,11 @@
 from imports import bag_to_numpy
 from visualization import visualize_animation
-from analysis import get_cluster_centers_per_frame, filter_clusters_1
+from analysis import (
+    get_cluster_centers_per_frame,
+    filter_clusters_1,
+    # select_N_brightest_per_frame,
+    # select_with_threshold,
+)
 from filter_clusters_2 import filter_clusters_2
 import numpy as np
 from pprint import pprint
@@ -11,9 +16,14 @@ filename = (
 )
 # filename = "labor_lidar.bag"
 frames = bag_to_numpy(filename)
-centers, visualization = get_cluster_centers_per_frame(frames)
+
+centers, visualization = get_cluster_centers_per_frame(
+    frames, rel_intensity_threshold=0.5, DBSCAN_epsilon=0.3, DBSCAN_min_samples=4
+)
 
 # chosen_centers = filter_clusters_1(centers, max_distance=0.3)
-chosen_centers = filter_clusters_2(centers, max_distance=0.15, min_velocity=0.08)
+chosen_centers = filter_clusters_2(
+    centers, max_distance=0.3, min_velocity=0.15, velocity_lookahead=5
+)
 
 visualize_animation(visualization, chosen_centers)
