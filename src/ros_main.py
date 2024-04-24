144990f84afe9126a8123cabbe2a43e93570dd46
Max Viehl <max.viehl@rwth-aachen.de>
2024-04-24 17:51:07 +0200

    WIP: continue with rosnode

--- src/ros_main.py
+++ src/ros_main.py
@@ -6,9 +6,10 @@ import pathlib
 import numpy as np
 import rospy
 from sensor_msgs import point_cloud2
+from geometry_msgs.msg import Transform
 
 from locate_reflector.find_cluster_centers import get_cluster_centers
-from locate_reflector.track_marker import track_marker, positions_from_indices
+from locate_reflector.track_marker import find_marker
 from transformation.calculate_transformation import filter_locations, calc_transformation
 
 
@@ -31,21 +32,21 @@ class OnlineCalibrator:
             topics.add(a)
             topics.add(b)
 
-        # create circular buffers
-        self.reflector_position_buffer: dict[str, collections.deque[np.ndarray]] = {
-            t: collections.deque(maxlen=int(params["velocity lookahead"]))
-            for t in topics
+        self.reflector_positions: dict[str, list[np.ndarray]] = {
+            t: [] for t in topics
         }
+        # circular buffer for frames
         self.frame_buffer: dict[str, collections.deque[np.ndarray]] = {
-            t: collections.deque(maxlen=int(params["velocity lookahead"]))
+            t: collections.deque(maxlen=int(params["window size"]))
             for t in topics
         }
 
-        # initialize ROS node and subscribe to sensors
-        rospy.init_node("online_calibration", anonymous=True)
+        # initialize ROS node, add publishers and subscribe to sensors
+        pub = rospy.Publisher("transformation", Transform, queue_size=10)
         for t in topics:
             # subscribe to all sensors
             rospy.Subscriber(t, point_cloud2, lambda pc2: self.on_message(t, pc2))
+        rospy.init_node("online_calibration", anonymous=True)
 
     def on_message(self, topic: str, pc2: point_cloud2):
         data = np.array(point_cloud2.read_points_list(pc2, skip_nans=False))
@@ -58,15 +59,16 @@ class OnlineCalibrator:
             DBSCAN_epsilon=params["DBSCAN epsilon"],
             DBSCAN_min_samples=int(params["DBSCAN min samples"])
         )
-        indices = track_marker(
+        search_result = find_marker(
             centers,
             max_distance=params["maximum neighbor distance"],
             min_velocity=params["minimum velocity"],
-            velocity_lookahead=int(params["velocity lookahead"]),
             max_vector_angle_rad=2 * np.pi * params["max. vector angle [deg]"] / 360,
-        )  # TODO this function will be changed!
-        # TODO somehow published a "target locked" state per sensor for live validation.
-        self.reflector_position_buffer[topic].append(positions_from_indices(indices, centers))
+        )  # TODO if a "target locked" state is returned from this function, publish it somewhere
+        if search_result:
+            reflector_pos, index = search_result
+            self.reflector_positions[topic].append(reflector_pos)
+
         # TODO
         #  deal with timing somehow: Currently, if one detector stops to send frames, its old frames
         #  will be used forever without throwing an error!
@@ -74,21 +76,21 @@ class OnlineCalibrator:
 
     def update_transformation(self):
         for sensors in self.sensor_pairs:
-            filtered = filter_locations(self.reflector_position_buffer, sensors)
+            filtered = filter_locations(self.reflector_positions, sensors)
             if len(filtered[sensors[0]]) >= 3:
                 # trafo calculation is possible
                 R, t = calc_transformation(filtered[sensors[0]], filtered[sensors[1]])
                 # TODO
                 #  use scipy instead
-                #  publish trafo + sensitivity
+                #  publish trafo + sensitivity, but only on changes
 
 
 if __name__ == '__main__':
     # load reflector tracking parameters
     paramfile = pathlib.Path(__file__).parent.parent.absolute() / "default_params.json"
-    # TODO look for replacement?
+    # TODO look for manually passed file/get from somewhere?
     with open(paramfile, "r") as f:
         params = json.load(f)
-    sensor_pairs = []  # TODO
+    sensor_pairs = []  # TODO get from somewhere
     OnlineCalibrator(params, sensor_pairs)
     rospy.spin()
