b42fb87711ce1c6ea499952612dc7a1a50fdd50d
Max Viehl <max.viehl@rwth-aachen.de>
2024-05-08 10:28:38 +0200

    Remove docker stuff from project

--- Docker/ros_lidar_docker/files/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-# setup ros environment, this is copied from the entrypoint script of the normal ROS Dockerfile
-source "/opt/ros/$ROS_DISTRO/setup.bash" --
-# also, source the robosense sdk
-source "/robosense/devel/setup.bash"
-
-exec "$@"
