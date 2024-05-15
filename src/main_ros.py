891d40e1f808424d6684be94bdcc542a1daa8bde
Max Viehl <max.viehl@rwth-aachen.de>
2024-05-15 13:43:55 +0200

    Implement timing-aware ROS online calibration for multiple sensor pairs

--- src/main_ros.py
+++ src/main_ros.py
@@ -1,122 +1,168 @@
 #!/usr/bin/env python
-import collections
 import json
 import pathlib
-from typing import Union
+from collections import deque
+from datetime import datetime, timedelta
 
 import numpy as np
 import rospy
-from geometry_msgs.msg import Transform, Vector3, Quaternion
+from geometry_msgs.msg import TransformStamped, Transform, Vector3, Quaternion
 from sensor_msgs import point_cloud2
-from std_msgs.msg import String
 
-from locate_reflector.find_cluster_centers import get_cluster_centers_single_frame
+from frame import Frame
 from locate_reflector.track_marker import find_marker_single_frame
-from transformation import filter_locations, calc_transformation_scipy
+from parameters import params
+from reflector_location import ReflectorLocation
+from transformation import calc_transformation_scipy, Transformation
+
+# Maximum age for a frame before it expires
+expiry_duration = timedelta(seconds=1 / float(params["sample_rate_Hz"]) / 2)
 
 
 class OnlineCalibrator:
-    def __init__(self, params: dict, sensor_pairs: list[tuple[str, str]]):
+    def __init__(self, sensor_pairs: list[tuple[str, str]]):
         """
-        Create an object to manage online calibration for multiple detectors.
-        Will subscribe to all relevant sensors and publish current calibration info
-        in a ROS topic as soon as it is available.
+        Create an object to manage online calibration for multiple Lidar sensors/sensor pairs.
+        Will subscribe to all relevant sensors and pass frames down to respective PairCalibrator instances.
+        These will then publish the current transformations as soon as they are available.
 
-        :param params: Parameter dict (as obtained from parameter JSON file)
         :param sensor_pairs: list with 2-tuples of sensor topics. One sensor may appear in multiple tuples.
-          Will subscribe to all specified sensor's ROS topics and calculate transformations for all those
-          sensor pairs if enough data is present.
         """
-        # extract unique topics from pairs
-        self.sensor_pairs = sensor_pairs
-        topics = set()
+        # extract unique topics from pairs and create pair calibrators
+        topics = set()  # collect the topics we have to subscribe to
         for a, b in sensor_pairs:
             topics.add(a)
             topics.add(b)
 
-        self.reflector_positions: dict[str, list[Union[None, np.ndarray]]] = {
-            t: [] for t in topics
-        }
-        # circular buffer for frames
-        self.frame_buffer: dict[str, collections.deque[np.ndarray]] = {
-            t: collections.deque(maxlen=int(params["window size"]))
-            for t in topics
-        }
-        # counts frame indices per sensor to avoid calculating transformation with different
-        # amount of frames received per sensor
-        self.frame_index: dict[str, int] = {
-            t: 0 for t in topics
+        # a dict which holds a list of interested paircalibrators per topic
+        self.pair_calibrators: dict[str, list[PairCalibrator]] = {
+            topic: [] for topic in topics
         }
 
         # initialize ROS node, add publishers and subscribe to sensors
-        self.trafo_pub = rospy.Publisher("transformation", Transform, queue_size=10)
-        self.state_pub = rospy.Publisher("calibration_state", String, queue_size=10)
-        # TODO get topic names from config
-
+        trafo_pub = rospy.Publisher("transformations", TransformStamped, queue_size=10)
         for t in topics:
             # subscribe to all sensors
             rospy.Subscriber(t, point_cloud2, lambda pc2: self.on_message(t, pc2))
         rospy.init_node("online_calibration", anonymous=True)
 
-    def on_message(self, topic: str, pc2: point_cloud2):
-        data = np.array(point_cloud2.read_points_list(pc2, skip_nans=False))
-        # TODO: fix performance problem. See `rosbag_to_numpy.py` as well.
-        self.frame_buffer[topic].append(data)
+        # create pair calibrators
+        for a, b in sensor_pairs:
+            pc = PairCalibrator(a, b, trafo_pub)
+            self.pair_calibrators[a].append(pc)
+            self.pair_calibrators[b].append(pc)
 
-        centers = get_cluster_centers_single_frame(
-            data,
-            rel_intensity_threshold=params["relative intensity threshold"],
-            DBSCAN_epsilon=params["DBSCAN epsilon"],
-            DBSCAN_min_samples=int(params["DBSCAN min samples"])
-        )
-        search_result, search_state = find_marker_single_frame(
+    def on_message(self, topic: str, pc2: point_cloud2):
+        data = np.array(point_cloud2.read_points_list_numpy(pc2, skip_nans=True))
+        frame = Frame(data, datetime.now())
+        # pass the new frame to all interested PairCalibrators, which will perform
+        # buffering and calculate a transformation if possible
+        for pc in self.pair_calibrators[topic]:
+            pc.new_frame(frame, topic)
+
+
+class PairCalibrator:
+
+    def __init__(self, topic1: str, topic2: str, trafo_publisher: rospy.Publisher):
+        self.frame_buffer_1: deque[Frame] = deque(maxlen=int(params["window size"]))
+        self.frame_buffer_2: deque[Frame] = deque(maxlen=int(params["window size"]))
+        self.topic1 = topic1
+        self.topic2 = topic2
+        self.last1: Frame | None = None
+        self.last2: Frame | None = None
+        self.reflector_locations_1: list[ReflectorLocation] = []
+        self.reflector_locations_2: list[ReflectorLocation] = []
+        self.transformation: Transformation | None = None
+        self.trafo_publisher = trafo_publisher
+
+    def new_frame(self, f: Frame, topic: str):
+        if topic == self.topic1:
+            self.last1 = f
+        else:
+            assert topic == self.topic2
+            self.last2 = f
+        # check if last frames are expired
+        if datetime.now() - self.last1.timestamp > expiry_duration:
+            self.last1 = None
+        if datetime.now() - self.last2.timestamp > expiry_duration:
+            self.last2 = None
+        if self.last1 is None or self.last2 is None:
+            return
+        # if we have frames for both sensors which are not expired, add them to buffer and calculate transformation
+        self.frame_buffer_1.append(self.last1)
+        self.frame_buffer_2.append(self.last2)
+        self.last1 = None
+        self.last2 = None
+
+        self.update_transformation()
+
+    @staticmethod
+    def calc_marker_location(buffer: deque[Frame]):
+        centers = [f.cluster_centers for f in buffer]
+        return find_marker_single_frame(
             centers,
             max_distance=params["maximum neighbor distance"],
             min_velocity=params["minimum velocity"],
             max_vector_angle_rad=2 * np.pi * params["max. vector angle [deg]"] / 360,
         )
-        self.frame_index[topic] += 1  # TODO somehow use timestamps
-        if search_result:
-            reflector_pos, index = search_result
-            self.reflector_positions[topic].append(reflector_pos)
-            self.update_transformation()
-            # a new transformation can not be calculated without new data, so only try if a new data point is added
-        else:
-            self.reflector_positions[topic].append(None)
-        self.state_pub.publish(String(search_state))
 
     def update_transformation(self):
-        index = None
-        for i in self.frame_index.values():
-            if index is not None and i != index:
-                # wait until number of frames is the same for each sensor
-                if abs(i - index) > 1:
-                    # TODO how to deal with this error?
-                    print("One sensor seems to drop frames!")
-                return
-            index = i
+        # first call calculate_marker_location of latest frames
+        result1, status1 = PairCalibrator.calc_marker_location(self.frame_buffer_1)
+        result2, status2 = PairCalibrator.calc_marker_location(self.frame_buffer_2)
+        # TODO do something with the status field...
 
-        del index
+        if result1 is None or result2 is None:
+            # Only continue if reflector is found in both new frames
+            return
 
-        for sensors in self.sensor_pairs:
-            filtered = filter_locations(self.reflector_positions, sensors)
-            if len(filtered[sensors[0]]) >= 3:
-                # trafo calculation is possible
-                a = filtered[sensors[0]]
-                b = filtered[sensors[1]]
-                R, t, Rq, sensitivity = calc_transformation_scipy(a, b)
+        # Save the obtained reflector locations
+        cluster1, index1 = result1
+        cluster1points = self.frame_buffer_1[-1].get_cluster_points(index1)
+        self.reflector_locations_1.append(ReflectorLocation(cluster1, cluster1points))
 
-                # publish
-                self.trafo_pub.publish(Transform(
-                    translation=Vector3(x=t[0], y=t[1], z=t[2]),
-                    rotation=Quaternion(x=Rq[0], y=Rq[1], z=Rq[2], w=Rq[3])
-                ))
+        cluster2, index2 = result2
+        cluster2points = self.frame_buffer_2[-1].get_cluster_points(index2)
+        self.reflector_locations_2.append(ReflectorLocation(cluster2, cluster2points))
+
+        # Recalculate and publish transformation with new data
+        P = np.ndarray([rl.cluster_mean[:3] for rl in self.reflector_locations_1])
+        Q = np.ndarray([rl.cluster_mean[:3] for rl in self.reflector_locations_2])
+        # TODO discuss how to calculate the single weight for each point pair?
+        weights = np.array([
+            min(rl1.weight, rl2.weight)
+            for rl1, rl2 in zip(self.reflector_locations_1, self.reflector_locations_2)
+        ])
+        self.new_transformation(calc_transformation_scipy(P, Q, weights))
+
+    def new_transformation(self, trafo: Transformation):
+        self.transformation = trafo
+        # http://docs.ros.org/en/api/geometry_msgs/html/msg/TransformStamped.html
+        # http://docs.ros.org/en/api/geometry_msgs/html/msg/Transform.html
+        self.trafo_publisher.publish(TransformStamped(
+            # Trafo is chosen such that it transforms P (frame1) to Q (frame2)
+            child_frame_id=self.topic1,
+            transform=Transform(
+                translation=Vector3(
+                    x=trafo.t[0],
+                    y=trafo.t[1],
+                    z=trafo.t[2]
+                ),
+                rotation=Quaternion(
+                    x=trafo.R_quat[0],
+                    y=trafo.R_quat[1],
+                    z=trafo.R_quat[2],
+                    w=trafo.R_quat[3]
+                )
+            )
+        ))
 
 
 if __name__ == '__main__':
     # load reflector tracking parameters
     paramfile = pathlib.Path(__file__).parent.parent.absolute() / "default_params.json"
     # TODO look for manually passed file/get from somewhere?
+    #  Integrate with topics from RSlidar-SDK config file and Docker container?
     with open(paramfile, "r") as f:
         params = json.load(f)
     sensor_pairs = []  # TODO get from somewhere
