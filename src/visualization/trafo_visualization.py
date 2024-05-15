27f3b3f74c4b17670e253966c8f876a12677f6eb
Tim Rehbronn <tim.rehbronn@rwth-aachen.de>
2024-05-15 13:36:00 +0200

    Lidar_Align_dev into Main

--- src/visualization/trafo_visualization.py
+++ src/visualization/trafo_visualization.py
@@ -2,9 +2,9 @@ import numpy as np
 import open3d as o3d
 
 colors = [
-    np.array([1.0, 0.0, 0.0]),  # RED
-    np.array([0.0, 1.0, 0.0]),  # GREEN
-    np.array([0.0, 0.0, 1.0]),  # BLUE
+    np.array([240, 185, 64]) / 255,  # RED
+    np.array([51, 105, 159]) / 255,  # GREEN
+    np.array([4, 114, 77]) / 255,  # BLUE
 ]
 
 
@@ -15,14 +15,17 @@ def np_to_pointcloud(frame_in):
     return pcd
 
 
-def visualize_trafo(points):
+def visualize_trafo(points, draw_point_match_markers=False):
     """
-    open open3d visualization for list of pointcloud data (numpy array, per point x y z).
+    Draws multiple point clouds in the same coordinate system.
     Does not apply transformations.
-    Draws different point clouds in different colors.
+    Draws each cloud in a different color.
 
-    :param points:
-    :return:
+    Optionally draws each 5th point in a gradient from black to white to make it easier
+    finding points with the same index. This is relevant to verify alignment of matched reflector locations.
+
+    :param points: list of numpy arrays containing the points of each point cloud
+    :param draw_point_match_markers: Toggles to color some points depending on their index, see above.
     """
     if len(points) > len(colors):
         # we are out of colors, add some in the `colors` list
@@ -30,6 +33,17 @@ def visualize_trafo(points):
     geoms = []
     for p, col in zip(points, colors[:len(points)]):
         geom = np_to_pointcloud(p)
-        geom.paint_uniform_color(col)
+        if draw_point_match_markers:
+            color_pointcloud_by_index(geom, col)
+        else:
+            geom.paint_uniform_color(col)
         geoms.append(geom)
     o3d.visualization.draw_geometries(geoms)
+
+
+def color_pointcloud_by_index(cloud, color, step=5):
+    l = len(cloud.points)
+    colors = np.tile(color, (l, 1))
+    for i in range(0, l, step):
+        colors[i] = np.array([1, 1, 1]) * (i / l)
+    cloud.colors = o3d.utility.Vector3dVector(colors)
