820d0634324fc95c73babf9eb0ac2bc93c02552c
Max Viehl <max.viehl@rwth-aachen.de>
2024-05-28 15:20:49 +0200

    Tidy up directory structure

--- lidar_align/setup.py
+++ lidar_align/setup.py
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            "main = lidar_align.src.main_ros:main"
+            "main = src.main_ros:main"
         ],
     },
 )
