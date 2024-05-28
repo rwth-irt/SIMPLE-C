fa34aa021b06b957b093b07108bfb4fa8d4ac39c
Max Viehl <max.viehl@rwth-aachen.de>
2024-05-28 15:34:55 +0200

    Update READMEs, remove superfluous/legacy files

--- lidar_align/setup.py
+++ lidar_align/setup.py
@@ -6,11 +6,6 @@ setup(
     name=package_name,
     version='0.0.0',
     packages=find_packages(exclude=['test']),
-    data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-    ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='root',
